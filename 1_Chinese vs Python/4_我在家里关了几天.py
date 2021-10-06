464da9ed3e6515c07ac7951ff6fd2eff1fe6f2ca
gu3vara <1607101779@qq.com>
2021-10-06 18:14:16 +0800

    10.6

---
+++ 1_Chinese vs Python/4_我在家里关了几天.py
@@ -0,0 +1,16 @@
+"""
+我在家里的时间 = 10
+for 我在家里的时间 in range (10,20):
+    print("我在家里待的第"+str(我在家里的时间)+"天")
+    我在家里的时间 = 我在家里的时间 + 1
+"""
+
+#打怪升级 level
+
+level = 10
+for level in range (10,20):
+    print("你现在等级："+str(level))
+    print("你现在应该去做主线任务")
+    print("任务完成，获得经验书！！！")
+    level = level + 1
+print("恭喜你升级到了"+str(level)+"级")
